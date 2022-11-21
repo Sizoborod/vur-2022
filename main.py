8a1ac15e56de8cd0110f7a35f69d7fd6784db565
sizoborod <se_t610@bk.ru>
2022-11-21 16:32:05 +0300

    Подготовил функции

--- main.py
+++ main.py
@@ -5,3 +5,26 @@ print('Hello world!')
 print('Привет яндекс!')
 print('sp1ker')
 print('ладно')
+
+
+def nikitin():
+    pass
+
+def alexsandrov():
+    pass
+
+def ivanov():
+    pass
+
+def mihailov():
+    pass
+
+def voshnyaev():
+    pass
+
+def burmistrova():
+    pass
+
+def yakovlev():
+    pass
+
